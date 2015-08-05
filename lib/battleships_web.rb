bbcc4d63803d1361f972bd43194826939a9d49af
lewis <ljones140@gmail.com>
2015-08-05 18:41:54 +0100

    board displays with dropdowns

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'battleships'
 
 class BattleshipsWeb < Sinatra::Base
 
@@ -22,11 +23,28 @@ class BattleshipsWeb < Sinatra::Base
   post '/game' do
     params[:name].empty? ? player_name = DEFAULT_PLAYER_NAME : player_name = params[:name]
     session[:player_name] = player_name
-    @name_display = "<h2 name=\"playername\">Player 1: #{player_name}</h2>" if player_name.length > 0
+    @name_display = "Player 1: #{player_name}" if player_name.length > 0
+    $game = Game.new Player, Board
+    if params[:fleet] != nil
+      ship = params[:fleet]
+      coordinate = params[:coordinate]
+      position = params[:position]
+      $game.player_1.place_ship(Ship.battleship, :B4, :vertically)
+    end
+    @board = $game.own_board_view($game.player_1)
     erb :game
   end
 
-
+  # get '/game' do
+  #   player_name = session[:player_name]
+  #   @name_display = "Player 1: #{player_name}" if session[:player_name].length > 0
+  #   ship = params[:fleet]
+  #   coordinate = params[:coordinate]
+  #   position = params[:position]
+  #   $game.player_1.place_ship(Ship.battleship, :B4, :vertically)
+  #   @board = $game.own_board_view($game.player_1)
+  #   erb :game
+  # end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -20,5 +20,37 @@ feature "Starting a new game" do
     expect(page).to have_content "Player 1: Anonymous"
   end
 
+  scenario " I see my board after game starts" do
+    visit "/newgame"
+    click_button "Submit"
+    expect(page).to have_content "your board"
+  end
+
+  scenario "I can see ships when game starts" do
+    visit "/newgame"
+    click_button "Submit"
+    expect(page).to have_select("fleet", selected: "destroyer")
+  end
+
+  scenario "I can enter coordinate" do
+    visit "/newgame"
+    click_button "Submit"
+    expect(page).to have_selector("input[type=text][name='coordinate']")
+  end
+
+  scenario "I can enter polarity" do
+    visit "/newgame"
+    click_button "Submit"
+    expect(page).to have_selector("input[type=text][name='direction']")
+  end
+
+  scenario "I can place a ship" do
+    visit "/newgame"
+    click_button "Submit"
+    select "destroyer", from: "fleet"
+    fill_in "direction", with: "A1"
+    click_button "Submit"
+    expect(page).to have content "Ship Placed at A1"
+  end
 end
 
